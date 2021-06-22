d0a86ec52fdcaacc72cfc312dec36416adb75b68
scootting <viadhose@hotmail.com>
2021-06-22 17:08:03 -0400

    pruebas

--- resources/js/views/Layout.vue
+++ resources/js/views/Layout.vue
@@ -1,19 +1,30 @@
 <template>
   <div>
-    <el-card class="box-card">
-      <h1>Hello, world!</h1>
+    <el-card class="box-card" style="text-align: justify">
+      <h1>Hola, Bienvenidos a la nueva plataforma!</h1>
       <p>
-        Lionel This is a template for a simple marketing or informational
-        website. It includes a large callout called a jumbotron and three
-        supporting pieces of content. Use it as a starting point to create
-        something more unique.
+        Como parte del compromiso de una mejora continua, tenemos el placer de
+        compartir con ustedes la actualización de nuestra página web.
+        <br /><br />La migración a la nueva plataforma otorga nueva tecnología
+        que nos ayuda a mejorar la experiencia de nuestros visitantes. Durante
+        los próximos meses, comenzará a ver cambios sutiles en el diseño del
+        sitio entre los que se incluyen modificaciones en la navegación, para
+        que se simplifique aún más la búsqueda de la información y el
+        lanzamiento de nuevas herramientas para asistirlos en sus necesidades
+        comerciales. <br /><br />Nuestro objetivo es mejorar su experiencia como
+        visitante y continuar facilitando la realización de sus tramites.
+        <br /><br />
+        Agradecemos sus sugerencias, y les pedimos que las compartan con
+        nosotros para seguir mejorando. Agradecemos su apoyo constante.
       </p>
+      <!--
       <el-button size="mini" plain @click="loadReportAssets()"
         >Default</el-button
       >
       <el-button type="primary" size="mini" plain>Primary</el-button>
       <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
       <el-button type="primary" icon="el-icon-share" size="mini"></el-button>
+      -->
     </el-card>
   </div>
 </template>
