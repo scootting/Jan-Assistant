35a1af67b906d97c13a48b4aae739a20ac0f41dc
scootting <viadhose@hotmail.com>
2021-07-30 11:22:35 -0400

    hola

--- app/FixedAsset.php
+++ app/FixedAsset.php
@@ -12,7 +12,8 @@ class FixedAsset extends Model
     //
     public static function GetDocumentFixedAssetByYear($year, $type){
         //$year = 2020;
-        $query = "select * from act.asignaciones aa where aa.tip_doc = '".$type."' and aa.gestion = '".$year."' and aa.estado = 'Verificado' order by aa.fec_cre desc";
+        //$query = "select * from act.asignaciones aa where aa.tip_doc = '".$type."' and aa.gestion = '".$year."' and aa.estado = 'Verificado' order by aa.fec_cre desc";
+        $query = "select * from act.asignaciones aa where aa.tip_doc in (1,3,2,4) and aa.gestion = '".$year."' and aa.estado = 'Verificado' order by aa.fec_cre desc";
         $data = collect(DB::select(DB::raw($query)));    
         return $data;
     }    
