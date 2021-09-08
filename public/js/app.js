3de0d2da731cc7da3a4eaf7e6baa2534a707b095
Nicole <yessnicolerp@gmail.com>
2021-09-08 10:55:16 -0400

    nada aqui

--- public/js/app.js
+++ public/js/app.js
@@ -102753,8 +102753,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Repository\Jan-Assistant\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Repository\Jan-Assistant\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\59178\Documents\ProyectoBienes2\Jan-Assistant\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\59178\Documents\ProyectoBienes2\Jan-Assistant\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
