f117a63b4ddd12e3ba9fc96c4af0dcde9b675d14
scootting <viadhose@hotmail.com>
2024-05-27 12:26:33 -0400

    holas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        If(env('APP_ENV') !== 'local') { $url->forceSchema('https'); }
+        /*
         if ($this->app->environment('production')) {
             \URL::forceScheme('https');
-        }
+        }*/
     }
 }
