5d489244814d4e1f86639c214734043b4dd3272d
scootting <viadhose@hotmail.com>
2026-02-04 09:04:41 -0400

    ouseha

--- app/Http/Controllers/TreasureController.php
+++ app/Http/Controllers/TreasureController.php
@@ -20,13 +20,16 @@ class TreasureController extends Controller
 
         switch ($typea) {
             case "Course":
+                    $typed = 'B';
+                /*
+                Gestion 2025. 
                 if(trim($nodip) == '8623347' or trim($nodip) == '10475961'  or trim($nodip) == '8655639'  or trim($nodip) == '8598295'  or trim($nodip) == '13165699' or
                    trim($nodip) == '8653318' or trim($nodip) == '8598916'  or trim($nodip) == '8554569'  or trim($nodip) == '12653174'  or trim($nodip) == '8523607' or                
                    trim($nodip) == '8645699'
                   )
                     $typed = 'X';
                 else
-                    $typed = 'B';
+                */
                 break;
             case "Sale":
                 $typed = 'U';
